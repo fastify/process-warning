a4f105927cddc3064e48326d1c3d92e37715d84b
delvedor <tommydelved@gmail.com>
2020-07-11 15:14:27 +0200

    Updated test

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 'use strict'
 
 const test = require('ava')
-const { createWarning, emitWarning, emittedWarnings } = require('./')
+const build = require('./')
 
 process.removeAllListeners('warning')
 
 test('Create warning with zero parameter', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'Not available')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'Not available')
   const err = new NewWarn()
   t.true(err instanceof Error)
   t.is(err.name, 'FastifyWarning')
@@ -15,7 +16,8 @@ test('Create warning with zero parameter', t => {
 })
 
 test('Create error with 1 parameter', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'hey %s')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'hey %s')
   const err = new NewWarn('alice')
   t.true(err instanceof Error)
   t.is(err.name, 'FastifyWarning')
@@ -24,7 +26,8 @@ test('Create error with 1 parameter', t => {
 })
 
 test('Create error with 2 parameters', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'hey %s, I like your %s')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'hey %s, I like your %s')
   const err = new NewWarn('alice', 'attitude')
   t.true(err instanceof Error)
   t.is(err.name, 'FastifyWarning')
@@ -33,7 +36,8 @@ test('Create error with 2 parameters', t => {
 })
 
 test('Create error with 3 parameters', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'hey %s, I like your %s %s')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'hey %s, I like your %s %s')
   const err = new NewWarn('alice', 'attitude', 'see you')
   t.true(err instanceof Error)
   t.is(err.name, 'FastifyWarning')
@@ -42,37 +46,42 @@ test('Create error with 3 parameters', t => {
 })
 
 test('Should throw when error code has no fastify name', t => {
+  const { create } = build()
   try {
-    createWarning()
+    create()
   } catch (err) {
     t.is(err.message, 'Fastify warning name must not be empty')
   }
 })
 
 test('Should throw when error has no code', t => {
+  const { create } = build()
   try {
-    createWarning('name')
+    create('name')
   } catch (err) {
     t.is(err.message, 'Fastify warning code must not be empty')
   }
 })
 
 test('Should throw when error has no message', t => {
+  const { create } = build()
   try {
-    createWarning('name', 'code')
+    create('name', 'code')
   } catch (err) {
     t.is(err.message, 'Fastify warning message must not be empty')
   }
 })
 
 test('FastifyWarning.toString returns code', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'foo')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'foo')
   const err = new NewWarn()
   t.is(err.toString(), 'FastifyWarning [CODE]: foo')
 })
 
 test('Create the warning without the new keyword', t => {
-  const NewWarn = createWarning('FastifyWarning', 'CODE', 'Not available')
+  const { create } = build()
+  const NewWarn = create('FastifyWarning', 'CODE', 'Not available')
   const err = NewWarn()
   t.true(err instanceof Error)
   t.is(err.name, 'FastifyWarning')
@@ -80,19 +89,42 @@ test('Create the warning without the new keyword', t => {
   t.is(err.code, 'CODE')
 })
 
-test.cb('emitWarning should emit a given code only once', t => {
+test.serial.cb('emit should emit a given code only once', t => {
   t.plan(4)
+  const { create, emit, emitted } = build()
+
   process.on('warning', onWarning)
   function onWarning (warning) {
     t.is(warning.name, 'FastifyDeprecation')
     t.is(warning.code, 'CODE')
     t.is(warning.message, 'Hello world')
-    t.true(emittedWarnings.get('CODE'))
+    t.true(emitted.get('CODE'))
   }
 
-  createWarning('FastifyDeprecation', 'CODE', 'Hello world')
-  emitWarning('CODE')
-  emitWarning('CODE')
+  create('FastifyDeprecation', 'CODE', 'Hello world')
+  emit('CODE')
+  emit('CODE')
+  setImmediate(() => {
+    process.removeListener('warning', onWarning)
+    t.end()
+  })
+})
+
+test.serial.cb('emit with interpolated string', t => {
+  t.plan(4)
+  const { create, emit, emitted } = build()
+
+  process.on('warning', onWarning)
+  function onWarning (warning) {
+    t.is(warning.name, 'FastifyDeprecation')
+    t.is(warning.code, 'CODE')
+    t.is(warning.message, 'Hello world')
+    t.true(emitted.get('CODE'))
+  }
+
+  create('FastifyDeprecation', 'CODE', 'Hello %s')
+  emit('CODE', 'world')
+  emit('CODE', 'world')
 
   setImmediate(() => {
     process.removeListener('warning', onWarning)
@@ -100,22 +132,12 @@ test.cb('emitWarning should emit a given code only once', t => {
   })
 })
 
-test.cb('emitWarning with interpolated string', t => {
-  t.plan(4)
-  process.on('warning', onWarning)
-  function onWarning (warning) {
-    t.is(warning.name, 'FastifyDeprecation')
-    t.is(warning.code, 'CODE')
-    t.is(warning.message, 'Hello world')
-    t.true(emittedWarnings.get('CODE'))
+test('Cannot reuse the same code more than once', t => {
+  const { create } = build()
+  create('FastifyWarning', 'CODE', 'Not available')
+  try {
+    create('FastifyWarning', 'CODE', 'Not available')
+  } catch (err) {
+    t.is(err.message, "The code 'CODE' already exist")
   }
-
-  createWarning('FastifyDeprecation', 'CODE', 'Hello %s')
-  emitWarning('CODE', 'world')
-  emitWarning('CODE', 'world')
-
-  setImmediate(() => {
-    process.removeListener('warning', onWarning)
-    t.end()
-  })
 })
