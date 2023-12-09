c262b361b6f2e7b515e00469fdb3172794defca7
Manuel Spigolon <manuel.spigolon@nearform.com>
2023-12-09 10:01:02 +0100

    fix: issue

---
+++ test/issue-88.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { test } = require('tap')
+const { createWarning } = require('..')
+
+test('Must not overwrite config', t => {
+  t.plan(1)
+
+  function onWarning (warning) {
+    t.equal(warning.code, 'CODE_1')
+  }
+
+  const a = createWarning('FastifyWarning', 'CODE_1', 'Msg', { unlimited: false })
+  createWarning('FastifyWarning', 'CODE_2', 'Msg', { unlimited: true })
+
+  process.on('warning', onWarning)
+  a.emit('CODE_1')
+  a.emit('CODE_1')
+
+  setImmediate(() => {
+    process.removeListener('warning', onWarning)
+    t.end()
+  })
+})
